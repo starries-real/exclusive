2e597cd69c5e6a7a3802286c23c5698f43c59d4f
starries-real <159205792+starries-real@users.noreply.github.com>
2025-04-15 18:38:45 +0700

    Create userdb.lua

---
+++ userdb.lua
@@ -0,0 +1,14 @@
+return {
+    users = {
+        {userid = 12345, discordid = "123456789", adminid = "admin1", addedAt = "2025-04-01 12:00:00"},
+        {userid = 67890, discordid = "987654321", adminid = "admin2", addedAt = "2025-04-02 14:30:00"}
+    },
+    admins = {
+        {userid = 1111, adminid = "admin1", permissions = {"all"}},
+        {userid = 2222, adminid = "admin2", permissions = {"all"}}
+    },
+    resellers = {
+        {userid = 3333, adminid = "reseller1", permissions = {"add_user"}},
+        {userid = 4444, adminid = "reseller2", permissions = {"add_user"}}
+    }
+}
